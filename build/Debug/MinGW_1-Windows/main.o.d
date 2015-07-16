988b9a68e1b25e283f0691809fb4095ba0df58ce
damian1baran <damian1baran@gmail.com>
2015-07-16 18:27:45 +0200

    initial commit

---
+++ build/Debug/MinGW_1-Windows/main.o.d
@@ -0,0 +1,22 @@
+build/Debug/MinGW_1-Windows/main.o: main.cpp \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql.h \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql_version.h \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql_com.h \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql_time.h \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/my_list.h \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/typelib.h \
+ ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/my_alloc.h
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql.h:
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql_version.h:
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql_com.h:
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/mysql_time.h:
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/my_list.h:
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/typelib.h:
+
+../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include/my_alloc.h:
---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,86 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=MinGW_1-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS="../../../../../Program Files/MySQL/MySQL Server 5.6/lib/libmysql.dll"  
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_mysql_test.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_mysql_test.exe: ../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/lib/libmysql.dll
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_mysql_test.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_mysql_test ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../../../../../Program\ Files/MySQL/MySQL\ Connector\ C++\ 1.1.5/include -I../../../../../Program\ Files/MySQL/MySQL\ Server\ 5.6/include -I/D/es/boost_1_55_0 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_mysql_test.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
